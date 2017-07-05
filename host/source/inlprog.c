0b2aac8b526ced94e5dd08ef9a5d7d542decf295
paul eeepc <paul@infiniteneslives.com>
2017-07-05 02:49:52 -0500

    Fixing compilation on linux.  Needed forward slashes on includes, and -lm for linking math libraries. Getting warning about lua's use of tmpnam, and to use mkstmp instead..  Not sure I care.

--- host/source/inlprog.c
+++ host/source/inlprog.c
@@ -24,9 +24,9 @@
 #include "shared_enums.h"
 
 //lua libraries
-#include "lua\lua.h"
-#include "lua\lauxlib.h"
-#include "lua\lualib.h"
+#include "lua/lua.h"
+#include "lua/lauxlib.h"
+#include "lua/lualib.h"
 
 void error (lua_State *L, const char *fmt, ...) {
 	va_list argp;
